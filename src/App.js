4fea9a95820f2f4a965e856c070062b5d173a98f
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-11 10:44:29 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a href="http://johannamagdalena.com/" target="_blank">
-          Johanna Magdalena
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/johannamag/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-        !
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a href="http://johannamagdalena.com/" target="_blank">
+            Johanna Magdalena
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/johannamag/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+          !
+        </footer>
+      </div>
     </div>
   );
 }
