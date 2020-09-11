de34b394c109b269596c259c29de36ba830f4cf9
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-11 10:12:53 +0200

    Added Github repo Url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,23 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="http://johannamagdalena.com/" target="_blank">
+          Johanna Magdalena
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/johannamag/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+        !
+      </footer>
+    </div>
+  );
 }
